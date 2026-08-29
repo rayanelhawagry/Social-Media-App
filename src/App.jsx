84826adf9a7ce788b78e345aa9f0a796e7f6f2dc
Rayan Elhawagry <se.mycareer@gmail.com>
2026-08-29 18:29:24 +0300

    Implement routing and layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import { useState } from 'react'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from './Layouts/MainLayout';
+import AuthLayout from './Layouts/AuthLayout';
+import FeedPage from './Pages/FeedPage';
+import Profile from './Pages/Profile';
+import PostDetails from './Pages/PostDetails';
+import NotFound from './Pages/NotFound';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+
+const router = createBrowserRouter([
+  {
+    path: '', element: <MainLayout />, children: [
+      { index: true, element: <FeedPage /> },
+      { path: 'profile', element: <Profile /> },
+      { path: 'post-details', element: <PostDetails /> },
+      { path: '*', element: <NotFound /> }
+    ]
+  },
+  {
+    path: '', element: <AuthLayout />, children: [
+      { path: 'login', element: <Login /> },
+      { path: 'register', element: <Register /> }
+    ]
+  }
+])
 
 function App() {
 
   return (
     <>
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Navbar from './../Components/Navbar';
+import Footer from './../Components/Footer';
+import { Outlet } from 'react-router-dom';
 
 export default function MainLayout() {
     return (
-        <div>MainLayout</div>
+        <>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </>
     )
 }
