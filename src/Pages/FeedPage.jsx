9233dcd48a9e94121e7953c843743a85421d9434
Rayan Elhawagry <se.mycareer@gmail.com>
2026-08-30 11:48:12 +0300

    HeroUI configuration

--- src/Pages/FeedPage.jsx
+++ src/Pages/FeedPage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Button } from '@heroui/react'
 
 export default function FeedPage() {
     return (
-        <div>FeedPage</div>
+        <>
+            <div>FeedPage</div>
+            <Button variant="danger-soft">Click</Button>
+        </>
     )
 }
