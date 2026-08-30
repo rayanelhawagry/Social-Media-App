c2728d4fb92f0ba2c4041a0e0bbc6a3ff65d0d90
Rayan Elhawagry <se.mycareer@gmail.com>
2026-08-30 15:41:12 +0300

    Handle form submit using react-hook-form

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,38 +1,58 @@
 import { Button, Input, ListBox, Select } from '@heroui/react'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 export default function Register() {
+    let { handleSubmit, register } = useForm({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: '',
+            rePassword: '',
+            dateOfBirth: '',
+            gender: ''
+        }
+    })
+
+    function sendData(userData) {
+        console.log(userData);
+    }
+
     return (
         <>
             <div className='bg-[#90CAF9] rounded-2xl shadow-2xl py-10 px-6 min-w-md'>
                 <h2 className='text-center mb-4 font-medium text-3xl'>Register Now!</h2>
-                <form className='flex flex-col gap-4'>
-                    <Input aria-label="Name" type='text' placeholder="Name" name='name' />
-                    <Input aria-label="Email" type='email' placeholder="Email" name='email' />
-                    <Input aria-label="Password" type='password' placeholder="Password" name='password' />
-                    <Input aria-label="RePassword" type='password' placeholder="Re-Password" name='rePassword' />
+                <form onSubmit={handleSubmit(sendData)} className='flex flex-col gap-4'>
+                    <Input aria-label="Name" type='text' placeholder="Name" {...register('name')} />
+                    <Input aria-label="Email" type='email' placeholder="Email" {...register('email')} />
+                    <Input aria-label="Password" type='password' placeholder="Password" {...register('password')} />
+                    <Input aria-label="RePassword" type='password' placeholder="Re-Password" {...register('rePassword')} />
                     <div className='flex justify-between gap-3'>
-                        <Input aria-label="DateOfBirth" type='date' className='w-[50%]' placeholder="Birth Date" name='dateOfBirth' />
-                        <Select placeholder="Select your gender" variant="primary" className='w-[50%]' name='gender'>
+                        <Input aria-label="DateOfBirth" type='date' className='w-[50%]' placeholder="Birth Date" {...register('dateOfBirth')} />
+                        <select {...register('gender')}>
+                            <option value="male">Male</option>
+                            <option value="female">female</option>
+                        </select>
+                        {/* <Select aria-label="Gender" placeholder="Select your gender" variant="primary" className='w-[50%]' name='gender'>
                             <Select.Trigger>
                                 <Select.Value />
                                 <Select.Indicator />
                             </Select.Trigger>
                             <Select.Popover>
-                                <ListBox>
-                                    <ListBox.Item key={'male'} textValue="Male">
+                                <ListBox {...register('gender')}>
+                                    <ListBox.Item value='male' textValue="Male">
                                         Male
                                         <ListBox.ItemIndicator />
                                     </ListBox.Item>
-                                    <ListBox.Item key={'female'} textValue="Female">
+                                    <ListBox.Item value='female' textValue="Female">
                                         Female
                                         <ListBox.ItemIndicator />
                                     </ListBox.Item>
                                 </ListBox>
                             </Select.Popover>
-                        </Select>
+                        </Select> */}
                     </div>
-                    <Button variant="tertiary" className='w-full'>Register</Button>
+                    <Button type='submit' variant="tertiary" className='w-full'>Register</Button>
                 </form>
             </div>
         </>
