579d431b4a3e0ea8ce5b57450988a30bca006288
Rayan Elhawagry <se.mycareer@gmail.com>
2026-09-17 09:48:00 +0300

    Implement Navbar component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <div>Navbar</div>
+        <>
+            <nav className="bg-neutral-primary fixed w-full z-20 top-0 inset-s-0 border-b border-default">
+                <div className="max-w-5xl flex items-center justify-between mx-auto p-4">
+                    <Link className="text-xl text-heading font-semibold">FakeIntimacy</Link>
+                    <div className="flex gap-6">
+                        <NavLink className="py-2 font-medium" to={'/login'}>Sign In</NavLink>
+                        <NavLink className="py-2 font-medium" to={'/register'}>Sign Up</NavLink>
+                        <div className="py-2 font-medium cursor-pointer">Log Out</div>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
